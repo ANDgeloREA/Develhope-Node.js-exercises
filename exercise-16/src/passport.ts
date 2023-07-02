1a362e4e6ba388988aa414bc63cd8323dfa63853
Andrea Angelo D'Onorio De Meo <andgelorea@gmail.com>
2023-07-03 00:14:59 +0200

    complete exercise 16

--- exercise-16/src/passport.ts
+++ exercise-16/src/passport.ts
@@ -1,7 +1,7 @@
-import * as dotenv from 'dotenv'
-dotenv.config()
-import passport from 'passport'
-import passportJWT from 'passport-jwt'
+import * as dotenv from 'dotenv';
+dotenv.config();
+import passport from 'passport';
+import passportJWT from 'passport-jwt';
 import { db } from './db';
 
 const { SECRET } = process.env;
--- exercise-16/src/server.ts
+++ exercise-16/src/server.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import "express-async-error";
 import morgan from "morgan";
 import { getAll, getOneById, create, updateById, deleteById, createImage } from "./controllers/planets.js";
+import { logIn, signUp } from "./controllers/users.js";
 import multer from "multer";
 
 const storage = multer.diskStorage({
@@ -33,6 +34,10 @@ app.put("/api/planets/:id", updateById);
 app.delete("/api/planets/:id", deleteById);
 
 app.post("/api/planets/:id/image", upload.single("image"), createImage);
+
+
+app.post("/api/users/login", logIn);
+app.post("/api/users/signup", signUp);
 
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`);
